90f2ba50e9e02109e93bf47fe92d65db579cea4b
Ansu Dangi <ansud.itpathsolutions@gmail.com>
2025-07-02 12:02:13 +0530

    change port

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -7,7 +7,7 @@ export const CONFIG = {
     MONGO_URL: process.env.MONGO_URL as string,
    
   },
-  PORT: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  PORT: process.env.PORT ? parseInt(process.env.PORT) : 3002,
   JWT_SECRET: process.env.JWT_SECRET || 'your-secret-key-here',
   JWT_EXPIRE: process.env.JWT_EXPIRE || '24h',
   ADMIN_EMAIL: process.env.EMAIL_FROM,
